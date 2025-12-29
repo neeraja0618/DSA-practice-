3a7c47efb31acbf61d3880ef3e1e620f4c9b160a
neeraja0618 <kovuruneeraja2@gmail.com>
2025-12-29 22:51:36 +0530

    stack basics

---
+++ stack_basics.py
@@ -0,0 +1,14 @@
+stack = []
+stack.append(10)
+stack.append(20)
+stack.append(30)
+print("Stack:", stack)
+stack.pop()
+print("After pop:", stack)
+
+#checking if stack is empty
+stack = []
+if not stack:
+    print("Stack is empty")
+else:
+    print("Stack is not empty")
